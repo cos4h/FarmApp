02a2a89e7f0de3cd12ab74513d74220f0c801199
r=cos(4θ) <olvadis2004@gmail.com>
2023-08-19 19:43:43 -0500

    to deploy3

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://farmapp-0coz.onrender.com/',
+  baseURL: 'https://farmapp-0coz.onrender.com/api',
   withCredentials: true
 })
 
